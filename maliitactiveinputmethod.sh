7b005ec1857a8010ef62cdc45bf7eb38cffd4db2
Marco Martin <notmart@gmail.com>
2012-05-21 14:39:42 +0200

    make sure the active plugin is enabled

--- maliitactiveinputmethod.sh
+++ maliitactiveinputmethod.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
-QML_IMPORT_PATH=/usr/lib/kde4/imports/
+#force the active im to be used
+gconftool-2 --set "/maliit/onscreen/enabled" --type list --list-type=string [nemo-keyboard.qml,en_us]
+gconftool-2 --set "/maliit/onscreen/active" --type list --list-type=string [nemo-keyboard.qml,en_us]
+
+#make sure org.kde.plasma.core can be imported
+export QML_IMPORT_PATH=/usr/lib/kde4/imports/
+
+#enable maliit plugin
 export QT_IM_MODULE=Maliit
 
