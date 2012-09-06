a77d1449de8489534c1b4b9d5e3b94a285f73306
Marco Martin <notmart@gmail.com>
2012-09-06 17:33:51 +0200

    gconf not used anymore

--- maliitactiveinputmethod.sh
+++ maliitactiveinputmethod.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-#force the active im to be used
-gconftool-2 --set "/maliit/onscreen/enabled" --type list --list-type=string [active-keyboard.qml,en_us]
-gconftool-2 --set "/maliit/onscreen/active" --type list --list-type=string [active-keyboard.qml,en_us]
-
 #make sure org.kde.plasma.core can be imported
 export QML_IMPORT_PATH=/usr/lib/kde4/imports/
 
